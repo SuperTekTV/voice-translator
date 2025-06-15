4890bd0d60dfb296b66f829cb38584fe4e9fa421
SuperTekTV <supertektv@gmail.com>
2025-06-15 12:09:28 +0200

    Configura cartella public per file statici

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/translate', async (req, res) => {
   const { q, source, target } = req.body;
