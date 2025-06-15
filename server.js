fe536930a00fc593fa3b310384dcec61d59f4fbc
SuperTekTV <supertektv@gmail.com>
2025-06-15 11:55:17 +0200

    Aggiornamento server per gestione porta dinamica

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch'); // npm install node-fetch@2
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
